5a0edf31a805a27d8d562277fd3c468ce5cb0644
Will Martin <jw.martin@outlook.com>
2025-07-10 09:45:50 -0400

    Add OBU setup documentation and scripts (#141)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    Documentation and scripts have been included to be added in the Cohda
    MK6 OBU.
    
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    
    ## Related Jira Key
    
    ARC-371
    
    ## Motivation and Context
    
    The added scripts are used to configure the OBU/RSU to work with this
    repository and run a check to ensure the configurations are correct
    every time the device boots. These changes are to address the following
    concerns:
    1. We want to version control our OBU configuration scripts.
    2. We have observed a recurring issue of multiple Cohda MK6 OBUs losing
    their configuration between power cycles.
    
    ## How Has This Been Tested?
    
    Tested on an MK6 OBU running RSU firmware. Tested alongside CARMA
    Messenger and the V2X ROS Driver to verify functionality.
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Defect fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [X] My change requires a change to the documentation.
    - [X] I have updated the documentation accordingly.
    - [X] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

---
+++ docs/mk6-rsu-obu-setup.sh
@@ -0,0 +1,301 @@
+#!/bin/bash
+
+##############################################################################
+# Default settings, no change needed if running this script locally
+##############################################################################
+#set -x 
+export ID0="rsu"
+export PW0="rsuadmin"
+export MIB_DIR="/home/duser/vm_share/fw_Release/docs/RSU/mibs/"
+export HOSTNAME=$(cat /etc/hostname)
+export SUT_IPV6_ADDR="::1"
+export IP="udp6:[$SUT_IPV6_ADDR]:161"
+export TX_MODE=0
+export CHAN=183
+
+# Change the following to the desired start and end dates for the WSMFwdRx table
+# Example: 2025-01-01 00:00 UTC = 07E901010000
+FW_StartDate="07E901010000"
+FW_EndDate="07EE01010000"
+# Change IP address and port to remote receiving address (spectra pc)
+REMOTE_IP="192.168.88.10"
+REMOTE_PORT="5398"
+
+##############################################################################
+# Detecting Environment
+##############################################################################
+_detect_host()
+{
+  if [ "$HOSTNAME" == "MKx-SDK" ]; then
+    export DIR="$MIB_DIR"
+  elif [[ $HOSTNAME =~ MK[5-6] ]]; then
+    FW_REV=$(fim -l | grep AR | awk '{print $3}')
+    echo "Running $FW_REV"
+    DIR="/mnt/rw/rsu1609/snmp/mibs/"
+  else
+    echo "Host not recognized"
+    exit 1
+  fi
+  export RW_AUTH_ARGS="-Le -t10 -r3 -v3 -lauthPriv -M $DIR -m RSU-MIB -u $ID0 -A $PW0 -X $PW0 -aSHA -xAES $IP"
+}
+
+##############################################################################
+# Local setup (file manipulation and settings)
+##############################################################################
+
+_edit_stack_conf()
+{
+#delete prior edits
+sed -i '0,/board/I!d' /mnt/rw/rsu1609/conf/stack.conf
+echo " " >> /mnt/rw/rsu1609/conf/stack.conf
+sync
+
+if [[ $HOSTNAME =~ MK5 ]]; then
+  echo "TxALogEnableFlag           = 1"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "RxALogEnableFlag           = 1"     >> /mnt/rw/rsu1609/conf/stack.conf
+fi
+
+if [[ $CHAN == 180 ]]; then
+  echo "WSMP_LLIF                  = 6"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "BSMPartITxRandOffset_ms    = 5"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "TxALogEnableFlag               = 1" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "TxBLogEnableFlag               = 1" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "RxALogEnableFlag               = 1" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "RxBLogEnableFlag               = 1" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_P1609PC5RxLogEnableFlag  = 0" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_P1609PC5TxLogEnableFlag  = 0" >> /mnt/rw/rsu1609/conf/stack.conf
+fi
+  echo "WSATxEnable                = 0"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "WBSS_WSA_RepeatRate        = 5"     >> /mnt/rw/rsu1609/conf/stack.conf
+
+  echo "Cohda_PCAP_LoggingDisabled = 0"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_DebugLevel           = 6"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_DebugTimeLevel       = 1"     >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_DebugInfoLevel       = 2"     >> /mnt/rw/rsu1609/conf/stack.conf
+
+  echo "SecurityEnable             = 1"     >> /mnt/rw/rsu1609/conf/stack.conf
+
+  echo "Cohda_Crypto_TestCountryCode = 840" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_Crypto_AeroLogging     = all" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_Crypto_SSPEVA   = $SSPEVA"    >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_Crypto_SSPSPAT  = $SSPSPAT"   >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "Cohda_Crypto_SSPMAP   = $SSPMAP"    >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "WSMP_ChannelNumber         = $CHAN" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "ContinuousChanNum          = $CHAN" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "ForcedSerChanNum           = $CHAN" >> /mnt/rw/rsu1609/conf/stack.conf
+  echo "ForcedcontrolChanNum       = $CHAN" >> /mnt/rw/rsu1609/conf/stack.conf
+}
+
+_manually_manipulate_rsu_files()
+{
+if [[ $HOSTNAME =~ MK[5-6] ]]; then
+  echo
+  echo "Performing manual setup that cannot be accomplished via SNMP"
+
+  echo "stopping application(s)"
+  systemctl disable cw-mkx-application.service && sync && systemctl stop cw-mkx-application
+  /opt/cohda/application/rc.local stop &>/dev/null
+  sync
+
+  #clean, add SNMP user
+  sed -i '0,/agentXTimeout/I!d' /mnt/rw/rsu1609/snmp/snmpd.conf
+  sync
+  net-snmp-config --create-snmpv3-user -A $PW0 -X $PW0 -a SHA -x AES $ID0 
+
+  _edit_stack_conf
+  rm -rf /mnt/rw/rsu1609/conf/user.conf 
+  sync
+
+  echo "starting application(s)"
+  /opt/cohda/application/rc.local start &>/dev/null
+  systemctl enable cw-mkx-application.service && sync && systemctl start cw-mkx-application
+  _detect_rsu1609_running
+fi
+}
+
+
+##############################################################################
+# Helper Functions
+##############################################################################
+_detect_rsu1609_running()
+{
+  sleep 5
+  RSU_NUM_PROCS=$(ps -A | egrep 'rsu1609|rsu-monitor' | wc -l)
+  until [ "$RSU_NUM_PROCS" = "2" ]; do
+    echo "Waiting for 'rsu1609', 'rsu-monitor'..."
+    sleep 1
+    RSU_NUM_PROCS=$(ps -A | egrep 'rsu1609|rsu-monitor' | wc -l)
+    done
+
+  sleep 1
+  sync  
+  echo "rsu1609 successfully started!"
+}
+
+_set_standby()
+{
+  until snmpget $RW_AUTH_ARGS rsuMode.0 | grep -q 'standby(2)'; do
+  snmpset $RW_AUTH_ARGS rsuMode.0 i 2 1>/dev/null
+  sleep 1
+  done
+}
+
+_set_operate()
+{
+  until snmpget $RW_AUTH_ARGS rsuMode.0 | grep -q 'operate(4)'; do
+  snmpset $RW_AUTH_ARGS rsuMode.0 i 4 1>/dev/null
+  sleep 1
+  done
+}
+
+_SYNC()
+{
+  echo "syncing..."
+  if [ "$HOSTNAME" == "MKx-SDK" ]; then
+    sshpass -p $PW0 ssh $ID0@$SUT_IPV6_ADDR -p 22 "sync && sync"
+  else
+    sync && sync
+  fi
+}
+
+_walk_the_mib()
+{
+echo " "
+echo "${FUNCNAME[0]} "
+snmpwalk $RW_AUTH_ARGS iso.0.15628.4.1 
+}
+
+_configure_IFM_simulation()
+{
+find . -name "cw_*.txt" 2>/dev/null | xargs sed -i "s/Signature=.*/Signature=False/"
+find . -name "cw_*.txt" 2>/dev/null | xargs sed -i "s/TxChannel.*/TxChannel=${CHAN}/"
+find . -name "cw_*.txt" 2>/dev/null | xargs sed -i "s/TxMode.*/TxMode=CONT/"
+sync
+}
+
+to_hex()
+{
+  local hex_ip="000000000000000000000000"
+  
+  # Split the IP address into its four octets
+  IFS='.' read -r -a octets <<< "$1"
+  
+  # Convert each octet to hex and concatenate
+  for octet in "${octets[@]}";
+  do
+      hex_ip+=$(printf '%02X' "$octet")
+  done
+
+  echo $hex_ip
+}
+
+##############################################################################
+# WSMFwdRx table
+##############################################################################
+# Convert the SUT IPv4 address to hex
+# The hex IP address is 32 characters long, with leading zeros
+# Example: 192.168.88.10 = 000000000000000000000000c0a8580a
+WSMFWD_ADDR=$(to_hex "$REMOTE_IP")
+if [ "${#WSMFWD_ADDR}" -ne 32 ]; then
+  echo "The submitted IP address is invalid. Exiting..."
+  exit 1
+fi
+echo "Configuring using backhaul IP address: $REMOTE_IP"
+echo "IP address in hex: $WSMFWD_ADDR"
+
+_destroy_WSMFwdRx()
+{
+echo " "
+echo "${FUNCNAME[0]} "
+for i in {9..1}; do
+  snmpset $RW_AUTH_ARGS rsuDsrcFwdStatus.$i i 6 1>/dev/null
+done
+}
+
+_set_WSMFwdRx() {
+  echo
+  echo "${FUNCNAME[0]} "
+
+  local psid_list=("0x20" "0x27" "0x8003" "0x8010" "0xBFEE")
+  local fwd_index=1
+
+  for psid in "${psid_list[@]}"; do
+    snmpset $RW_AUTH_ARGS \
+      rsuDsrcFwdPsid.$fwd_index        x "$psid" \
+      rsuDsrcFwdDestIpAddr.$fwd_index  x "$WSMFWD_ADDR" \
+      rsuDsrcFwdDestPort.$fwd_index    i "$REMOTE_PORT" \
+      rsuDsrcFwdProtocol.$fwd_index    i 2 \
+      rsuDsrcFwdRssi.$fwd_index        i -100 \
+      rsuDsrcFwdMsgInterval.$fwd_index i 1 \
+      rsuDsrcFwdDeliveryStart.$fwd_index x "$FW_StartDate" \
+      rsuDsrcFwdDeliveryStop.$fwd_index  x "$FW_EndDate" \
+      rsuDsrcFwdEnable.$fwd_index      i 1 \
+      rsuDsrcFwdStatus.$fwd_index      i 4
+    echo
+
+    ((fwd_index++))
+  done
+}
+
+##############################################################################
+# Immediate Forward table 
+##############################################################################
+
+_destroy_IFM()
+{
+echo
+echo "${FUNCNAME[0]} "
+for i in {9..1}; do
+  snmpset $RW_AUTH_ARGS rsuIFMStatus.$i i 6
+done
+}
+
+_set_IFM() {
+  echo
+  echo "${FUNCNAME[0]} "
+
+  local psid_list=("0x20" "0xBFEE" "0x8003" "0x8010")
+  local ifm_index=1
+
+  for psid in "${psid_list[@]}"; do
+    snmpset $RW_AUTH_ARGS \
+      rsuIFMPsid.$ifm_index       x "$psid" \
+      rsuIFMDsrcMsgId.$ifm_index  i 0 \
+      rsuIFMTxMode.$ifm_index     i $TX_MODE \
+      rsuIFMTxChannel.$ifm_index  i $CHAN \
+      rsuIFMEnable.$ifm_index     i 1 \
+      rsuIFMStatus.$ifm_index     i 4
+    echo
+    
+    ((ifm_index++))
+  done
+}
+
+##############################################################################
+# Main
+##############################################################################
+echo " "
+_configure_IFM_simulation
+_detect_host
+_manually_manipulate_rsu_files
+_SYNC
+
+_set_standby
+_destroy_WSMFwdRx
+_set_operate
+
+_set_standby
+_set_WSMFwdRx
+_set_operate
+
+_set_standby
+_destroy_IFM
+_set_operate
+
+_set_standby
+_set_IFM
+_set_operate
+
+_walk_the_mib
+
+exit 0
